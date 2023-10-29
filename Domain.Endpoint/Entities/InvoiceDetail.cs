1782fbbfb4a0233eb0c397e8f271d8891d670aca
DlopezS98 <01dlopezs98@gmail.com>
2023-10-28 23:10:16 -0600

    Add entities - restaurant example

---
+++ Domain.Endpoint/Entities/InvoiceDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Domain.Endpoint.Entities
+{
+    public class InvoiceDetail : BaseEntity, IHaveCreationData
+    {
+        public Guid InvoiceId { get; set; }
+        public Guid? ProductDetailId { get; set; }
+        public Guid? DishId { get; set; }
+        public string ItemType { get; set; }
+        public int Quantity { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal Discount { get; set; }
+        public decimal Total { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid CreatedBy { get; set; }
+
+        public virtual Invoice Invoice { get; set; }
+    }
+}
