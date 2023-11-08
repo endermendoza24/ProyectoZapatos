1daf18c0c64a67b3b8e49b7631b921b60e9276ee
endermendoza24 <enderalonso24@gmail.com>
2023-11-07 20:27:35 -0600

    Todo funcionando, agregando DTOS y Entidades

---
+++ Domain.Endpoint/Entities/Color.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Endpoint.Entities
+{
+    public class Color : BaseEntity
+    {
+        public string ID_COLOR { get; set; }
+        public string NOMBRE_COLOR { get; set; }
+    }
+}
